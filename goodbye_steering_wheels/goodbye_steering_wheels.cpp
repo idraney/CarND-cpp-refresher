0d4ae60a23ef52b87f8ddc16291faa4d6bc88564
idraney <ian.draney@oit.edu>
2020-04-18 21:52:33 -0700

    updated goodbye steering wheels

---
+++ goodbye_steering_wheels/goodbye_steering_wheels.cpp
@@ -0,0 +1,11 @@
+// Simple Print
+
+#include <iostream>
+
+int main() {
+  // write your code here!
+  // print "no more steering wheels" to stdout
+  // std::cout << "no more steering wheels";
+  std::cout << "No more steering wheels!" << std::endl;
+  return 0;
+}
