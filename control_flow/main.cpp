06c3c7f77487ab5459b416b4372cdc9fdb6af520
idraney <ian.draney@oit.edu>
2020-04-18 22:14:24 -0700

    contains for loops defined by function in a separate header file

---
+++ control_flow/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * No need to change this file (unless you want to change the test case below).
+ */
+
+#include <iostream>
+#include "PrintString.h"
+
+int main() {
+  PrintString("This is a test.", 10);
+
+  return 0;
+}
